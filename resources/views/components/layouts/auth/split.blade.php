1f128177f7673d1b22b8c72ca5ffcff07184cb05
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2025-07-31 13:36:50 +0300

    4th commit

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -3,41 +3,32 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
-                <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
+<body class="rounded-full bg-radial-[at_50%_50%] from-zinc-900 to-white dark:from-white dark:to-[#151611] to-75%">
+        <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC]  rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none">
+                    <a href="{{ route('login') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
+                    <span class="flex h-30 w-30 mb-1 items-center justify-center rounded-md hidden dark:flex">
+                        <x-dark-app-logo-icon class="size-100 text-black dark:text-white" />
                     </span>
-                    {{ config('app.name', 'Laravel') }}
+                    <span class="flex h-30 w-30 mb-1 items-center justify-center rounded-md dark:hidden">
+                        <x-app-logo-icon class="size-100 text-black dark:text-white" />
+                    </span>
+                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
                 </a>
-
-                @php
-                    [$message, $author] = str(Illuminate\Foundation\Inspiring::quotes()->random())->explode('-');
-                @endphp
-
-                <div class="relative z-20 mt-auto">
-                    <blockquote class="space-y-2">
-                        <flux:heading size="lg">&ldquo;{{ trim($message) }}&rdquo;</flux:heading>
-                        <footer><flux:heading>{{ trim($author) }}</flux:heading></footer>
-                    </blockquote>
-                </div>
-            </div>
-            <div class="w-full lg:p-8">
-                <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                    <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
-                        <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                            <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
-                        </span>
-
-                        <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
-                    </a>
+                <div class="flex flex-col gap-6">
                     {{ $slot }}
                 </div>
-            </div>
+                </div>
+                <div class="bg-gradient-to-r from-white to-zinc-900  dark:bg-gradient-to-r dark:from-[#151611] dark:to-white relative lg:-ms-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-e-lg! aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden ">
+                    <img
+                        src="{{ asset('image/car.png') }}"
+                        alt="car Image"
+                        class="w-120 mx-auto mt-30 object-contain"
+                        loading="lazy"
+                    >
+            </main>
         </div>
         @fluxScripts
-    </body>
+    </body>
 </html>
